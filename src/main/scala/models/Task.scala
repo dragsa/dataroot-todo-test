5e9cf38744e9b116f96fbe7bbb18853c55cc7f39
Andrey Gnatyuk <to.gnatuk@gmail.com>
2017-11-25 23:22:25 +0200

    todo: models and table creation code added

---
+++ src/main/scala/models/Task.scala
@@ -0,0 +1,58 @@
+package models
+
+import slick.jdbc.PostgresProfile.api._
+import slick.lifted.Tag
+
+import scala.concurrent.Future
+
+case class Task(taskName: String, taskBody: Option[String] = None, taskOwner: Int, taskId: Option[Int] = None)
+
+final class TaskTable(tag: Tag)
+  extends Table[Task](tag, "tasks") {
+
+  def taskName = column[String]("task_name")
+  def taskBody = column[String]("task_body")
+  def taskOwner = column[Int]("task_owner")
+  def taskId = column[Int]("task_ID", O.PrimaryKey, O.AutoInc)
+
+  def userFk =
+    foreignKey("user_fk_ID", taskOwner, TableQuery[UserTable])(_.userId)
+
+  def uniqueTaskPerUser = index("task_user_unique", (taskName, taskOwner), unique = true)
+
+  def * =
+    (taskName, taskBody.?, taskOwner, taskId.?) <> (Task.apply _ tupled, Task.unapply)
+}
+
+object TaskTable {
+  val table = TableQuery[TaskTable]
+}
+
+class TaskRepository(implicit db: Database) {
+  val taskTableQuery = TaskTable.table
+
+  def createOne(task: Task): Future[Task] = {
+    db.run(taskTableQuery returning taskTableQuery += task)
+  }
+
+  def createMany(tasks: List[Task]): Future[Seq[Task]] = {
+    db.run(taskTableQuery returning taskTableQuery ++= tasks)
+  }
+
+  def updateOne(task: Task): Future[Int] = {
+    db.run(
+      taskTableQuery
+        .filter(_.taskId === task.taskId)
+        .update(task))
+  }
+
+  def deleteOne(taskId: Int): Future[Int] = {
+    db.run(taskTableQuery.filter(_.taskId === taskId).delete)
+  }
+
+  def getById(taskId: Int): Future[Option[Task]] = {
+    db.run(
+      taskTableQuery.filter(_.taskId === taskId).result.headOption)
+  }
+}
+
---
+++ src/main/scala/models/User.scala
@@ -0,0 +1,53 @@
+package models
+
+import slick.jdbc.PostgresProfile.api._
+import slick.lifted.Tag
+
+import scala.concurrent.Future
+
+case class User(taskName: String, userPass: String, userId: Option[Int] = None)
+
+final class UserTable(tag: Tag)
+  extends Table[User](tag, "users") {
+
+  val userName = column[String]("user_name", O.Unique)
+  // TODO salt this?
+  val userPass = column[String]("user_pass")
+  val userId = column[Int]("user_ID", O.PrimaryKey, O.AutoInc)
+
+  def * =
+    (userName, userPass, userId.?) <> (User.apply _ tupled, User.unapply)
+
+}
+
+object UserTable {
+  val table = TableQuery[UserTable]
+}
+
+class UserRepository(implicit db: Database) {
+  val userTableQuery = UserTable.table
+
+  def createOne(user: User): Future[User] = {
+    db.run(userTableQuery returning userTableQuery += user)
+  }
+
+  def createMany(users: List[User]): Future[Seq[User]] = {
+    db.run(userTableQuery returning userTableQuery ++= users)
+  }
+
+  def updateOne(user: User): Future[Int] = {
+    db.run(
+      userTableQuery
+        .filter(_.userId === user.userId)
+        .update(user))
+  }
+
+  def deleteOne(userId: Int): Future[Int] = {
+    db.run(userTableQuery.filter(_.userId === userId).delete)
+  }
+
+  def getById(userId: Int): Future[Option[User]] = {
+    db.run(
+      userTableQuery.filter(_.userId === userId).result.headOption)
+  }
+}
