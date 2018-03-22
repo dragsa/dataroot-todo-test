1b8daf0a62d7f77da0cf1754115270149a5c5ab7
Andrey Gnatyuk <to.gnatuk@gmail.com>
2018-03-23 00:52:45 +0200

    todo: common vals are now initialized in trait

--- src/test/scala/LoginTest.scala
+++ src/test/scala/LoginTest.scala
@@ -1,16 +1,17 @@
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 
 import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
 
 class LoginTest extends Specification {
   sequential
 
-  //TODO add init data for each test case
+  //TODO factor out data for each test case
   // specs2 context?
 
-  "login as root -> logout -> exit" >> {
-    val loginData = "login -u root -p root\nlogout\nexit"
-    System.setIn(new ByteArrayInputStream(loginData.getBytes()))
+  "login as root -> logout -> exit" in new CommandsStack {
+    cmds.append("login -u root -p root\nlogout\nexit")
+    System.setIn(new ByteArrayInputStream(cmds.toString.getBytes))
     val outContent = new ByteArrayOutputStream()
     Console.withOut(outContent) {
       org.todo.gnat.Main.main(Array())
@@ -20,14 +21,19 @@ class LoginTest extends Specification {
     outContent.toString must contain("Bye-bye!")
   }
 
-  "logout before login" >> {
-    val logoutData = "logout\nexit"
-    System.setIn(new ByteArrayInputStream(logoutData.getBytes()))
+  "logout before login" in new CommandsStack {
+    cmds.append("logout\nexit")
+    System.setIn(new ByteArrayInputStream(cmds.toString.getBytes))
     val outContent = new ByteArrayOutputStream()
     Console.withOut(outContent) {
       org.todo.gnat.Main.main(Array())
     }
     outContent.toString must contain("you need to login first")
+    outContent.toString must contain("Bye-bye!")
+  }
+
+  trait CommandsStack extends Scope {
+    lazy val cmds = new java.lang.StringBuilder()
   }
 
 }
